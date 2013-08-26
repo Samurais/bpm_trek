36dca6a0f892890eee558200e8a41a2b8c5750eb
Samurais <hailiang.hl.wang@gmail.com>
2013-08-26 10:38:24 +0800

    add jars for lib

--- bpmn-examples/src/main/java/org/bpmnwithactiviti/chapter10/ldap/LDAPConnectionUtil.java
+++ bpmn-examples/src/main/java/org/bpmnwithactiviti/chapter10/ldap/LDAPConnectionUtil.java
@@ -5,22 +5,26 @@ import org.apache.directory.ldap.client.api.LdapConnection;
 
 public class LDAPConnectionUtil {
 
-	public static LdapConnection openConnection(LDAPConnectionParams connectionParams) {
-		LdapConnection connection = new LdapConnection(connectionParams.getLdapServer(), connectionParams.getLdapPort());
-	  try {
-	    connection.bind(connectionParams.getLdapUser(), connectionParams.getLdapPassword());
-    } catch (Exception e) {
-	    throw new ActivitiException("LDAP connection open failure", e);
-    }
-	  return connection;
+	public static LdapConnection openConnection(
+			LDAPConnectionParams connectionParams) {
+		LdapConnection connection = new LdapConnection(
+				connectionParams.getLdapServer(),
+				connectionParams.getLdapPort());
+		try {
+			connection.bind(connectionParams.getLdapUser(),
+					connectionParams.getLdapPassword());
+		} catch (Exception e) {
+			throw new ActivitiException("LDAP connection open failure", e);
+		}
+		return connection;
 	}
-	
+
 	public static void closeConnection(LdapConnection connection) {
 		try {
-	  	connection.unBind();
-	  	connection.close();
-	  } catch (Exception e) {
-	    throw new ActivitiException("LDAP connection close failure", e);
-    }
+			connection.unBind();
+			connection.close();
+		} catch (Exception e) {
+			throw new ActivitiException("LDAP connection close failure", e);
+		}
 	}
 }
